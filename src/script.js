e786b8726b89ca6bfd65e0e6e323d24efd70bc5b
Капралов Яков Владиславович <70843236+prorockk@users.noreply.github.com>
2021-10-11 18:50:49 +0400

    fix stairs

--- src/script.js
+++ src/script.js
@@ -1,15 +1,16 @@
 const options = {
     width: window.innerWidth,
-    height: window.innerWidth/2.17,
+    height: window.innerWidth / (1390 / 640),
 }
 
 const app = new PIXI.Application(options);
 
 const getWindowParam = () => {
     app.view.width = window.innerWidth;
-    app.view.height = window.innerWidth/2.17;
-    app.stage.scale.set(window.innerWidth/1390);
+    app.view.height = window.innerWidth / (1390 / 640);
+    app.stage.scale.set(window.innerWidth / 1390);
     document.body.style.paddingTop = `${(window.innerHeight-app.view.height) / 2}px`;
+    document.body.style.margin = '0px';
 }
 window.onresize = getWindowParam;
 
@@ -22,7 +23,7 @@ function setup () {
     const background = getSprite('back');
     const logo = getSprite('logo', [30, 5]);
 
-    const btn = getSprite('btn', [682, 556]);
+    const btn = getSprite('btn', [682, 560]);
     btn.anchor.set(0.5);
     btn.interactive = true;
     btn.on('click', () => window.location.href = 'https://play.google.com/store/apps/details?id=com.playrix.homescapes');
@@ -45,10 +46,10 @@ function setup () {
         getSprite('small_plants', [1150, 160]),
     )
     
-    const plant3 = getSprite('plant3', [1122, 440]);
+    const plant3 = getSprite('plant3', [1130, 450]);
     const Austin = getSprite('Austin', [695, 113]);
     const oldStair = getSprite('oldStair', [830, 50]);
-    const hammer = getSprite('icon_hammer', [1090, 255]);
+    const hammer = getSprite('icon_hammer', [1087, 258]);
 
 
     setTimeout(()=> {
@@ -80,8 +81,8 @@ function getSprite (name, coordinate) {
 
 function renderMenu (stairs) {
     const menu = new PIXI.Container();
-    menu.x = 850;
-    menu.y = 10;
+    menu.x = 841;
+    menu.y = 4;
 
     const selected = getSprite('choosed', [10, 5]);
 
@@ -99,21 +100,21 @@ function renderMenu (stairs) {
     variant1.interactive = true;
     variant1.on('click', () => {
         renderNewStair(stairs, variant1, 1, ok, selected);
-    })
+    });
 
     const variant2 = new PIXI.Container();
     variant2.x = 130;
     variant2.interactive = true;
     variant2.on('click', () => {
         renderNewStair(stairs, variant2, 2, ok, selected);
-    })
+    });
 
     const variant3 = new PIXI.Container();
-    variant3.x = 260;
+    variant3.x = 257;
     variant3.interactive = true;
     variant3.on('click', () => {
         renderNewStair(stairs, variant3, 3, ok, selected);
-    })
+    });
 
     const back1 = getSprite('circle');
     const stair1 = getSprite('01', [30, -30]);
@@ -135,26 +136,18 @@ function renderMenu (stairs) {
 function renderNewStair(stairContainer, variantContainer, variant, ok, selected) {
     stairContainer.removeChildren();
     const newStair = getSprite(`new_stair_0${variant}`);
-    newStair.alpha = 0;
-    let okScale = 0;
-    ok.scale.set(1, okScale);
-    app.ticker.add(() => {
-        okScale += 0.05;
-        okScale.toFixed(1);
-        if (newStair.alpha < 1) newStair.alpha += 0.03;
-        if (okScale < 1) ok.scale.set(1, okScale);
-    });
-
     const newStairMenu = getSprite(`0${variant}`); 
 
     switch (variant) {
         case 1:
             newStair.x = 870;
+            newStair.y = 5;
             newStairMenu.x = 30;
             newStairMenu.y = -30;
             break;
         case 2:
             newStair.x = 870;
+            newStair.y = 20;
             newStairMenu.x = 30;
             newStairMenu.y = -70;
             break;
@@ -165,6 +158,21 @@ function renderNewStair(stairContainer, variantContainer, variant, ok, selected)
             newStairMenu.y = -40;
             break;
     }
+
+    newStair.alpha = 0;
+    let okScale = 0;
+    ok.scale.set(1, okScale);
+    newStair.y -= 20;
+    app.ticker.add(() => {
+        okScale += 0.05;
+        okScale.toFixed(1);
+        if (newStair.alpha < 1) newStair.alpha += 0.03;
+        if (okScale < 1) {
+            ok.scale.set(1, okScale);
+            newStair.y += 1;
+        }
+    });
+
     variantContainer.addChildAt(selected, 1);
     variantContainer.addChild(ok);
     stairContainer.addChild(newStair);
@@ -188,6 +196,6 @@ function renderFinal() {
         overlayAlpha > 1 ? overlay.alpha = 1 : overlay.alpha = overlayAlpha;
     });
     
-    app.stage.addChildAt(final, app.stage.children.length-2);
-    app.stage.addChildAt(overlay, app.stage.children.length-3);
+    app.stage.addChildAt(final, app.stage.children.length - 2);
+    app.stage.addChildAt(overlay, app.stage.children.length - 3);
 }
